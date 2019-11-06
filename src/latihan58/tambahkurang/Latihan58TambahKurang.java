7c88d7a74c4dead43f40e14e00ae5f5892b97c3f
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-11-06 15:40:49 +0700

    Add files via upload

---
+++ src/latihan58/tambahkurang/Latihan58TambahKurang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan58.tambahkurang;
+
+/**
+ *
+ * @author Aero
+ */
+public class Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+  JumlahBilangan jBilangan = new JumlahBilangan();
+        jBilangan.tampilHasilJumlah();
+        
+        SelisihBilangan sBilangan = new SelisihBilangan();
+        sBilangan.tampilSelisih();
+    }
+    
+}
